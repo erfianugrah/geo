0833eb8da137a7cf7259e8b7c7ab7bfc0f609662
Erfi Anugrah <erfi.anugrah@gmail.com>
2021-06-12 09:39:16 +0800

    HTML in response v2

--- geo.js
+++ geo.js
@@ -3,6 +3,11 @@ addEventListener("fetch", (event) => {
 })
 
 async function handleRequest(request) {
+
+    let newResponse = await fetch(request)
+    const requestHeaders = JSON.stringify(Object.fromEntries(request.headers), null, 2)
+    const responseHeaders = JSON.stringify(Object.fromEntries(newResponse.headers), null, 2)
+
     let html_style = "body {padding:6em; font-family: sans-serif;} h1 {color:#f6821f} h2 {color:#f6821f}"
     
     let list = ''
@@ -44,10 +49,6 @@ async function handleRequest(request) {
         <h2>Response Headers</h2>
             <pre>${responseHeaders}</pre>
         </body>`
-
-    let newResponse = await fetch(request)
-    const requestHeaders = JSON.stringify(Object.fromEntries(request.headers), null, 2)
-    const responseHeaders = JSON.stringify(Object.fromEntries(newResponse.headers), null, 2)
 
     let response = new Response(html, newResponse.body, newResponse)
     response.headers.set("cf-edge-cache", "no-cache")
